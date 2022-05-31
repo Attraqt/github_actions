7b097ad9b50791d1c4f5ad35353fc3a413c53ff6
Ambroise Mullie <amullie@attraqt.com>
2022-05-31 14:51:18 +0200

    :bug: import * as [...] instead of import []

--- .github/actions/check-deployment-status/dist/index.js
+++ .github/actions/check-deployment-status/dist/index.js
@@ -6,21 +6,41 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
-const github_1 = __importDefault(__nccwpck_require__(5438));
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
 const request_1 = __nccwpck_require__(6234);
 function defaultParse(inputName) {
-    const inputValue = core_1.default.getInput(inputName);
+    const inputValue = core.getInput(inputName);
     if (!inputValue) {
         if (inputName === 'owner') {
-            return github_1.default.context.repo.owner;
+            return github.context.repo.owner;
         }
         if (inputName === 'repo') {
-            return github_1.default.context.repo.repo;
+            return github.context.repo.repo;
         }
     }
     return inputValue || undefined;
@@ -46,12 +66,12 @@ requestWithAuth('GET /repos/{owner}/{repo}/deployments/{deployment_id}/statuses'
     .then(result => {
     console.log('result', result);
     if (result && result.data) {
-        core_1.default.setOutput('data', result.data);
+        core.setOutput('data', result.data);
     }
 })
     .catch(error => {
     console.log('error', error);
-    core_1.default.setFailed(error.message);
+    core.setFailed(error.message);
 });
 
 
